117013777f3897e88202106a51bd5b456fd1e415
Minizbot2012 <Mini@zbots.org>
2020-11-08 01:15:55 -0500

    Initial commit

---
+++ SynWeaponKeywords/Types/WeaponKeywords.cs
@@ -0,0 +1,13 @@
+namespace WeaponKeywords.Types {
+    public class WeaponDB {
+        public string? mod;
+        public string? keyword;
+        public string[]? commonNames;
+        public string? outputDescription;
+        public string? animation;
+    }
+    public class ExcludesDB {
+        public string[]? phrases;
+        public string[]? weapons;
+    }
+}
