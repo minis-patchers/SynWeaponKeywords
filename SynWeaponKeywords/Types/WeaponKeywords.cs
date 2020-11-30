0a28ba25e96c5a370e0980260f0aa8b29ff93054
Minizbot2012 <bradleyfwin@gmail.com>
2020-11-30 18:44:18 -0500

    first version with extra keyword support (TEST)

--- SynWeaponKeywords/Types/WeaponKeywords.cs
+++ SynWeaponKeywords/Types/WeaponKeywords.cs
@@ -1,9 +1,16 @@
 namespace WeaponKeywords.Types {
     public class WeaponDB {
+        //Mod which contains keyword
         public string? mod;
+        //Mod / main keyword
         public string? keyword;
+        //akeywords don't require specific mods
+        public string[]? akeywords;
+        //Common names of item (partial match)
         public string[]? commonNames;
+        //descriptor when patched
         public string? outputDescription;
+        //unused
         public string? animation;
     }
     public class ExcludesDB {
