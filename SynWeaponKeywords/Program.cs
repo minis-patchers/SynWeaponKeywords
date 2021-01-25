064cf7cccb0bcbfe051641d1c9ebb25610adca1a
Minizbot2012 <mini@zbots.org>
2021-01-25 17:45:10 -0500

    Clean Code, muta/syn update

--- SynWeaponKeywords/Program.cs
+++ SynWeaponKeywords/Program.cs
@@ -15,36 +15,51 @@ namespace WeaponKeywords
     {
         public static async Task<int> Main(string[] args)
         {
-            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences() {
-                    ActionsForEmptyArgs = new RunDefaultPatcher()
-                    {
-                        IdentifyingModKey = "WeapTypeKeywords.esp",
-                        TargetRelease = GameRelease.SkyrimSE,
-                    }
+            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences()
+            {
+                ActionsForEmptyArgs = new RunDefaultPatcher()
+                {
+                    IdentifyingModKey = "WeapTypeKeywords.esp",
+                    TargetRelease = GameRelease.SkyrimSE,
+                }
             });
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            try {
+            try
+            {
                 var database = JObject.Parse(File.ReadAllText(Path.Combine(state.ExtraSettingsDataPath, "database.json"))).ToObject<Database>();
                 Dictionary<string, List<IKeywordGetter>> formkeys = new Dictionary<string, List<IKeywordGetter>>();
-                foreach(var (key, value) in database.DB)
+                foreach (var (key, value) in database.DB)
                 {
-                    foreach(var src in database.sources)
+                    foreach (var src in database.sources)
                     {
-                        if(value.keyword==null) continue;
-                        var keywords  = state.LoadOrder.PriorityOrder.Keyword().WinningOverrides()
-                            .Where(x=>x.FormKey.ModKey == src)
-                            .Where(x=>value.keyword.Contains(x.EditorID??""));
-                        foreach(var keyword in keywords) {
-                            if(keyword == null) continue;
-                            if(!formkeys.ContainsKey(key)) formkeys[key] = new List<IKeywordGetter>();
+                        if (value.keyword == null)
+                        {
+                            continue;
+                        }
+
+                        var keywords = state.LoadOrder.PriorityOrder.Keyword().WinningOverrides()
+                            .Where(x => x.FormKey.ModKey == src)
+                            .Where(x => value.keyword.Contains(x.EditorID ?? ""));
+                        foreach (var keyword in keywords)
+                        {
+                            if (keyword == null)
+                            {
+                                continue;
+                            }
+
+                            if (!formkeys.ContainsKey(key))
+                            {
+                                formkeys[key] = new List<IKeywordGetter>();
+                            }
+
                             formkeys[key].Add(keyword);
                         }
                     }
                 }
-                foreach(var weapon in state.LoadOrder.PriorityOrder.Weapon().WinningOverrides())
+                foreach (var weapon in state.LoadOrder.PriorityOrder.Weapon().WinningOverrides())
                 {
                     var edid = weapon.EditorID;
                     var nameToTest = weapon.Name?.String?.ToLower();
@@ -54,35 +69,44 @@ namespace WeaponKeywords
                         .ToArray();
                     var globalExclude = database.excludes.phrases
                         .Any(ph => nameToTest?.Contains(ph) ?? false) ||
-                        database.excludes.weapons.Contains(edid??"");
-                    if(database.includes.ContainsKey(edid ?? ""))
+                        database.excludes.weapons.Contains(edid ?? "");
+                    if (database.includes.ContainsKey(edid ?? ""))
                     {
                         var nw = state.PatchMod.Weapons.GetOrAddAsOverride(weapon);
-                        if(formkeys.ContainsKey(database.includes[edid ?? ""]))
+                        if (formkeys.ContainsKey(database.includes[edid ?? ""]))
                         {
-                            foreach(var keyform in formkeys[database.includes[edid ?? ""]]) {
-                                if(!(nw.Keywords?.Contains(keyform)??false)) {
+                            foreach (var keyform in formkeys[database.includes[edid ?? ""]])
+                            {
+                                if (!(nw.Keywords?.Contains(keyform) ?? false))
+                                {
                                     nw.Keywords?.Add(keyform.FormKey);
                                     Console.WriteLine($"{nameToTest} is {database.DB[database.includes[edid ?? ""]].outputDescription}, adding {keyform.EditorID} from {keyform.FormKey.ModKey}");
                                 }
                             }
-                        } else {
-                            Console.WriteLine($"{nameToTest} is {database.DB[database.includes[edid??""]].outputDescription}, but not changing (missing esp?)");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{nameToTest} is {database.DB[database.includes[edid ?? ""]].outputDescription}, but not changing (missing esp?)");
                         }
                     }
-                    if(matchingKeywords.Length > 0 && !globalExclude)
+                    if (matchingKeywords.Length > 0 && !globalExclude)
                     {
                         Console.WriteLine($"{nameToTest}: \n\tMatching Keywords: {String.Join(",", matchingKeywords)}");
                         var nw = state.PatchMod.Weapons.GetOrAddAsOverride(weapon);
-                        foreach(var kyd in matchingKeywords)
+                        foreach (var kyd in matchingKeywords)
                         {
-                            if(formkeys.ContainsKey(kyd) && !(database.DB[kyd].exclude.Any(cn => nameToTest?.Contains(cn) ?? false))) 
+                            if (formkeys.ContainsKey(kyd) && !database.DB[kyd].exclude.Any(cn => nameToTest?.Contains(cn) ?? false))
                             {
                                 Console.WriteLine($"\t{nw.Name}: {nw.EditorID} from {nw.FormKey.ModKey} is {database.DB[kyd].outputDescription} adding: ");
-                                foreach(var keyform in formkeys[kyd]) 
+                                foreach (var keyform in formkeys[kyd])
                                 {
-                                    if(database.DB[kyd].excludeSource.Contains(keyform.FormKey.ModKey.FileName)) continue;
-                                    if(!(nw.Keywords?.Contains(keyform.FormKey)??false)) {
+                                    if (database.DB[kyd].excludeSource.Contains(keyform.FormKey.ModKey.FileName))
+                                    {
+                                        continue;
+                                    }
+
+                                    if (!(nw.Keywords?.Contains(keyform.FormKey) ?? false))
+                                    {
                                         nw.Keywords?.Add(keyform.FormKey);
                                         Console.WriteLine($"\t\tAdded keyword {keyform.EditorID} from {keyform.FormKey.ModKey}");
                                     }
@@ -91,7 +115,9 @@ namespace WeaponKeywords
                         }
                     }
                 }
-            } catch {
+            }
+            catch
+            {
                 throw new Exception($"Database Error, try deleting {state.ExtraSettingsDataPath}");
             }
         }
