64440531833f2f20f1b60362e376a72130597a9e
Minizbot2012 <bradleyfwin@gmail.com>
2024-09-29 05:34:53 -0400

    simplify names

--- SynPatcher/Datapacks.cs
+++ SynPatcher/Datapacks.cs
@@ -3,9 +3,9 @@ using Newtonsoft.Json;
 namespace MZCommon.Datapack;
 
 public static class DatapackStatics {
-    public static HttpClient Cli = new();
+    public static readonly HttpClient Cli = new();
     public static PKGInfo GetPKG(string URI) {
-        var tsk = DatapackStatics.Cli.GetStringAsync(URI);
+        var tsk = Cli.GetStringAsync(URI);
         tsk.Wait();
         return JsonConvert.DeserializeObject<PKGInfo>(tsk.Result);
     }
@@ -14,9 +14,9 @@ public static class DatapackStatics {
 public struct PKGInfo {
     public string Root;
     public HashSet<string> Files;
-    public string GetFile(string File) {
-        if(!this.Files.Contains(File)) return string.Empty;
-        var tsk = DatapackStatics.Cli.GetStringAsync($"{this.Root}/{File}");
+    public readonly string GetFile(string file) {
+        if(!Files.Contains(file)) return string.Empty;
+        var tsk = DatapackStatics.Cli.GetStringAsync($"{Root}/{file}");
         tsk.Wait();
         return tsk.Result;
     }
